f299d0fb030670cb88bd492e687ae715db677675
Dostoyewski <dostoyewski@yandex.ru>
2020-11-29 00:32:50 +0300

    Added Bot model

--- voice_bot/models.py
+++ voice_bot/models.py
@@ -1,1 +1,39 @@
-# Create your models here.
+from django.db import models
+
+LANG = (
+    (0, "Русский"),
+    (1, "English"),
+    (2, "Deutsch"),
+    (3, "Francias")
+)
+
+
+# TODO Make decorator to register Bot for some page
+class Bot(models.Model):
+    """
+    Bot model
+    Used to make voice bot
+    """
+    # Bot descriprion, optional
+    description = models.CharField(max_length=1000, blank=True)
+    # Language selection
+    language = models.IntegerField(choices=LANG)
+    # Name
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + ", " + "Language: " + self.get_language()
+
+    def get_language(self):
+        """
+        Returns language code
+        :return: lang code
+        """
+        if self.language == 0:
+            return 'ru-RU'
+        elif self.language == 1:
+            return 'en-US'
+        elif self.language == 2:
+            return 'de-DE'
+        elif self.language == 3:
+            return 'fr-FR'
